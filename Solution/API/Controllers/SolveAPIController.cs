8e193cbbd377e463345aec6914236463b550b799
Joris Pevcevicius <joris@pevcas.eu>
2023-09-23 18:09:47 +0300

    add image support and simplify question storage

--- Solution/API/Controllers/SolveAPIController.cs
+++ Solution/API/Controllers/SolveAPIController.cs
@@ -10,6 +10,7 @@ namespace API.Controllers
     public class SolveAPIController : ControllerBase
     {
         private static readonly string _questionsFolder = "../../questions";
+		private static readonly string _questionDBExtension = ".json";
         private readonly ILogger<SolveAPIController> _logger;
 		private static Random random = new Random();
 
@@ -21,26 +22,14 @@ namespace API.Controllers
         [HttpGet("GetQuestion/{question}")]
         public APIResultWithData<QuestionModel> GetQuestion(string question)
         {
-            // string[] imageFiles = Directory.GetFiles(questionFolder, "*.jpg");
-
-            // if (imageFiles.Length == 0)
-            //     return NotFound("No image file found in the selected question subfolder");
-
-            // string questionImageFile = imageFiles[0]; // Assuming only one image file per subfolder
-            // byte[] imageBytes = System.IO.File.ReadAllBytes(questionImageFile);
-
-            // Find the corresponding answer text file (assuming the name matches)
-
-			// verify all question characters are alphanumeric
 			if(!question.All(c => char.IsAsciiLetterOrDigit(c) || c == '_' || c == '-'))
 				return APIResultWithData<QuestionModel>.CreateFailure("Question name should only contain alphanumeric characters, dashes and underscores");
 
-            string questionModelFile = Path.Combine(_questionsFolder, question, "question.json");
+            string questionModelFile = Path.Combine(_questionsFolder, question + ".json");
 
             if (!System.IO.File.Exists(questionModelFile))
                 return APIResultWithData<QuestionModel>.CreateFailure("Answer file not found for the selected question");
 
-            // Read the answer from the text file
             string questionModelText = System.IO.File.ReadAllText(questionModelFile);
 
 			var questionModel = JsonConvert.DeserializeObject<QuestionModel>(questionModelText);
@@ -50,18 +39,23 @@ namespace API.Controllers
 		[HttpGet("GetRandomQuestionName")]
 		public APIResultWithData<string> GetRandomQuestionName()
 		{
-			string[] questionSubfolders = Directory.GetDirectories(_questionsFolder).Select(s => new DirectoryInfo(s).Name).ToArray();
+			DirectoryInfo dirinfo = new DirectoryInfo(_questionsFolder);
+			string[] questionFiles = (from file in dirinfo.GetFiles()
+												where file.Name.EndsWith(_questionDBExtension)
+												select file.Name.Substring(0, file.Name.Length - _questionDBExtension.Length))
+												.ToArray();
 
-			if(questionSubfolders.Length == 0)
+
+			if(questionFiles.Length == 0)
 				return APIResultWithData<string>.CreateFailure("No questions found");
 
 			int randomIndex;
 			lock(random)
 			{
-				randomIndex = random.Next(0, questionSubfolders.Length);
+				randomIndex = random.Next(0, questionFiles.Length);
 			}
 
-			string questionFolder = questionSubfolders[randomIndex];
+			string questionFolder = questionFiles[randomIndex];
 			return APIResultWithData<string>.CreateSuccess(questionFolder);
 		}
 
