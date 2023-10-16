eeffa11ab99b1e4ffba4c26b272a2a95c91d227a
dasv2003 <daniil.shvager@gmail.com>
2023-10-16 23:38:50 +0300

    included optional argument

--- Solution/MVC/Models/SolveResultViewModel.cs
+++ Solution/MVC/Models/SolveResultViewModel.cs
@@ -4,16 +4,16 @@ namespace MVC.Models
 {
     public class SolveResultViewModel
     {
-        public SolveResultViewModel(QuestionModelWithAnswer questionModel, string questionName, string roomId, int selectedOption)
+        public SolveResultViewModel(QuestionModelWithAnswer questionModel, string questionName, string roomId, int? wrongAnswerIndex = null)
         {
             Question = questionModel;
             QuestionName = questionName;
             RoomId = roomId;
-            SelectedOption = selectedOption;
+            WrongAnswerIndex = wrongAnswerIndex;
         }
         public QuestionModelWithAnswer Question { get; set; }
         public string? QuestionName { get; set; }
         public string? RoomId { get; set; }
-        public int SelectedOption { get; set; }
+        public int? WrongAnswerIndex { get; set; }
     }
 }
