d36f30fbd6a7677e794a398c7ef71dbff1d4c894
dasv2003 <daniil.shvager@gmail.com>
2023-11-19 20:19:46 +0200

    added more questionAPI tests

--- Solution/BrainBoxAPI.Tests/Controller/QuestionAPIControllerTests.cs
+++ Solution/BrainBoxAPI.Tests/Controller/QuestionAPIControllerTests.cs
@@ -27,6 +27,48 @@ namespace BrainBoxAPI.Tests.Controller
             _controller = new QuestionAPIController(_logger, _mapper, _questionRepo);
         }
 
+        [Fact]
+        public async Task GetQuestion_ReturnsOkResult_WithValidId()
+        {
+            // Arrange
+            var questionId = 1;
+
+            var questionModel = new QuestionModel
+            {
+                Id = 1,
+                Title = "Just Question",
+                ImageSource = null,
+            };
+
+            A.CallTo(() => _questionRepo.GetById(A<int>._))
+                .Returns(Task.FromResult(questionModel));
+
+            var expectedDto = new QuestionDTO
+            {
+                Id = questionModel.Id,
+                Title = questionModel.Title,
+                ImageSource = questionModel.ImageSource,
+            };
+            A.CallTo(() => _mapper.Map<QuestionDTO>(A<QuestionModel>._))
+                .Returns(expectedDto);
+
+            // Act
+            var result = await _controller.GetQuestion(questionId);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+
+            var okResult = result as OkObjectResult;
+            okResult.Should().NotBeNull();
+
+            
+            okResult.Value.Should().NotBeNull(); 
+
+            var returnedDto = okResult.Value.Should().BeOfType<QuestionDTO>().Subject;
+            returnedDto.Should().BeEquivalentTo(expectedDto); 
+        }
+
+
 
         [Fact]
         public async Task GetFullQuestion_ReturnsOkResult_WithValidId()
@@ -44,25 +86,32 @@ namespace BrainBoxAPI.Tests.Controller
             A.CallTo(() => _questionRepo.GetById(A<int>._))
                 .Returns(Task.FromResult(questionModel));
 
-            A.CallTo(() => _mapper.Map<QuestionDTO>(A<QuestionModel>._))
-                .Returns(new QuestionDTO
-                {
-                    Id = questionModel.Id,
-                    Title = questionModel.Title,
-                    ImageSource = questionModel.ImageSource
-                });
+            var expectedDto = new QuestionWithAnswerDTO
+            {
+                Id = questionModel.Id,
+                Title = questionModel.Title,
+                ImageSource = questionModel.ImageSource,
+            };
+            A.CallTo(() => _mapper.Map<QuestionWithAnswerDTO>(A<QuestionModel>._))
+                .Returns(expectedDto);
 
             // Act
             var result = await _controller.GetFullQuestion(questionId);
 
             // Assert
-
             result.Should().BeOfType<OkObjectResult>();
 
             var okResult = result as OkObjectResult;
             okResult.Should().NotBeNull();
 
-            //MORE
+            okResult.Value.Should().NotBeNull(); 
+
+          
+            var returnedDto = okResult.Value.Should().BeOfType<QuestionWithAnswerDTO>().Subject;
+            returnedDto.Should().BeEquivalentTo(expectedDto); 
         }
+
+
+
     }
 }
