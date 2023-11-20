aa165afc842c40186fb59d253c762b31da2d2c09
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2023-11-20 23:17:38 +0200

    setup BrainBoxUI testing

--- Solution/BrainBoxUI.Tests/Controller/LobbyControllerTests.cs
+++ Solution/BrainBoxUI.Tests/Controller/LobbyControllerTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BrainBoxUI.Controllers;
+using BrainBoxUI.Helpers.API;
+using Microsoft.AspNetCore.Mvc;
+using SharedModels.Question;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,36 @@ namespace BrainBoxUI.Tests.Controller
 {
     public class LobbyControllerTests
     {
-        
+        private IApiRepository _apiRepository;
+
+        private LobbyController _controller;
+        public LobbyControllerTests()
+        {
+            _apiRepository = A.Fake<IApiRepository>();
+
+            _controller = new LobbyController(_apiRepository);
+        }
+
+
+        [Fact]
+        public void AllRooms_FetchesListOfRoomDTO_ReturnsViewWithRooms()
+        {
+            // Arrange
+            var fakeRooms = A.Fake<List<RoomDTO>>();
+
+            APIError? apiError;
+            A.CallTo(() => _apiRepository.Get<List<RoomDTO>>(A<string>._, out apiError)).Returns(fakeRooms);
+
+            // Act
+            var result = _controller.AllRooms();
+
+            // Assert
+            result.Should().BeOfType<ViewResult>();
+            var viewResult = result as ViewResult;
+            viewResult.Model.Should().BeEquivalentTo(fakeRooms);
+        }
+
     }
+
+
 }
