e4fae7818c6b2271fa993fe0207819118267f440
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2023-12-03 17:42:21 +0200

    remove Identity and Entity services from Program.cs
    required removal of MapRazorPages to run, otherwise Unable to connect to web server 'https'

--- Solution/BrainBoxUI/Program.cs
+++ Solution/BrainBoxUI/Program.cs
@@ -1,18 +1,8 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using BrainBoxUI.Data;
 using BrainBoxUI.Helpers.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IApiRepository, ApiRepository>();
@@ -41,6 +31,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
 app.Run();
