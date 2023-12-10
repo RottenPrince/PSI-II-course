d88ac393375b7abeffa7ecb038051b2f5aaf0739
dasv2003 <daniil.shvager@gmail.com>
2023-12-10 19:49:48 +0200

    initial  user API testing class

---
+++ Solution/BrainBoxAPI.Tests/ControllerUnitTests/UserAPIControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BrainBoxAPI.Caching;
+using BrainBoxAPI.Controllers;
+using BrainBoxAPI.Data;
+using BrainBoxAPI.Managers;
+using BrainBoxAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SharedModels.Lobby;
+using SharedModels.Question;
+
+namespace BrainBoxAPI.Tests.ControllerUnitTests
+{
+    internal class UserAPIControllerTests
+    {
+    }
+}
